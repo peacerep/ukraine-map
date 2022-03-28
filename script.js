c82b8c5fcbf926ee3143f03454065cc6f5579520
Sarah Schöttler <sarah@schoettler.email>
2022-03-28 16:36:10 +0100

    show 1000 UCDP events – need to add additional pages

--- script.js
+++ script.js
@@ -11,5 +11,39 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 // show the scale bar on the lower left corner
 L.control.scale({ imperial: true, metric: true }).addTo(map);
 
-// show a marker on the map
-L.marker({ lon: 0, lat: 0 }).bindPopup("The center of the world").addTo(map);
+d3.json(
+  "https://ucdpapi.pcr.uu.se/api/gedevents/21.1?pagesize=1000&StartDate=2020-01-01"
+).then(function (data) {
+  console.log(data);
+
+  // let ucdp = data.Result;
+
+  // if (data.TotalPages < 20) {
+  //   for (let i = 0; i < 3; i++) {
+  //     let nextPage =
+  //       "https://ucdpapi.pcr.uu.se/api/gedevents/21.1?pagesize=1000&StartDate=2020-01-01&page=" +
+  //       i;
+  //     d3.json(nextPage).then(function (d) {
+  //       console.log(d);
+  //     });
+  //   }
+  //   // doesn't work
+  //   console.log(ucdp);
+  //   // }
+  // } else {
+  //   console.log(
+  //     "Trying to load too much data at once, page count: ",
+  //     data.TotalPages
+  //   );
+  // }
+
+  // remove loading message when data is loaded
+  d3.select("#loading-message").attr("class", "hidden");
+
+  // add a marker w/ popup for each event
+  data.Result.forEach(function (event) {
+    L.marker({ lon: event.longitude, lat: event.latitude })
+      .bindPopup(event.where_description)
+      .addTo(map);
+  });
+});
